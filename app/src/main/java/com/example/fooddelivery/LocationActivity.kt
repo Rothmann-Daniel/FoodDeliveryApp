833cb908258fad01865f703e2606b7bc255ab7c9
Daniel Rotman <danilarothmann@gmail.com>
2025-07-02 22:15:45 +0700

    add SingUpUserActivity, LocationActivity + layouts

--- app/src/main/java/com/example/fooddelivery/LocationActivity.kt
+++ app/src/main/java/com/example/fooddelivery/LocationActivity.kt
@@ -1,35 +1,20 @@
 package com.example.fooddelivery
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.fooddelivery.databinding.ActivitySingUpUserBinding
 
-class SingUpUserActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivitySingUpUserBinding
-
+class LocationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySingUpUserBinding.inflate(layoutInflater)
-        setContentView(binding.root)
         enableEdgeToEdge()
+        setContentView(R.layout.activity_location)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        binding.tvQuestionSingUp.setOnClickListener {
-            val intent = Intent(this, LoginUserActivity::class.java)
-            startActivity(intent)
-        }
-
-        binding.tvSupportSingUp.setOnClickListener {
-            EmailUtils.sendSupportEmail(this)
         }
     }
 }
--- app/src/main/java/com/example/fooddelivery/LoginUserActivity.kt
+++ app/src/main/java/com/example/fooddelivery/LoginUserActivity.kt
@@ -24,41 +24,15 @@ class LoginUserActivity : AppCompatActivity() {
             insets
         }
 
-        binding.tvSupport.setOnClickListener {
-            sendSupportEmail()
+        binding.tvSupportSingIn.setOnClickListener {
+            EmailUtils.sendSupportEmail(this)
         }
 
-        binding.tvQuestionLUA.setOnClickListener {
+        binding.tvQuestionSingIn.setOnClickListener {
             val intent = Intent(this, SingUpUserActivity::class.java)
             startActivity(intent)
         }
 
     }
 
-
-    private fun sendSupportEmail() {
-        val emailIntent = Intent(Intent.ACTION_SEND).apply {
-            type = "message/rfc822"
-            putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.support_email)))
-            putExtra(Intent.EXTRA_SUBJECT, getString(R.string.support_email_subject))
-            putExtra(Intent.EXTRA_TEXT, getString(R.string.support_email_body))
-        }
-
-        when {
-            packageManager.queryIntentActivities(emailIntent, 0).isEmpty() -> {
-                Toast.makeText(this, R.string.no_email_app, Toast.LENGTH_LONG).show()
-            }
-
-            else -> try {
-                startActivity(
-                    Intent.createChooser(
-                        emailIntent,
-                        getString(R.string.choose_email_app)
-                    )
-                )
-            } catch (e: Exception) {
-                Toast.makeText(this, R.string.email_send_error, Toast.LENGTH_LONG).show()
-            }
-        }
-    }
 }
