d4a0732c767720e7a57202878b15f91aa0be1b03
Daniel Rotman <danilarothmann@gmail.com>
2025-07-02 15:46:48 +0700

    add layout activity_login

--- app/src/main/java/com/example/fooddelivery/LoginUserActivity.kt
+++ app/src/main/java/com/example/fooddelivery/LoginUserActivity.kt
@@ -1,35 +1,20 @@
 package com.example.fooddelivery
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.fooddelivery.databinding.ActivityStartBinding
 
-class StartActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityStartBinding
-
+class LoginUserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityStartBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
         enableEdgeToEdge()
-
-        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        binding.btNextStartActivity.setOnClickListener {
-            val intent = Intent(this, LoginUserActivity::class.java)
-            startActivity(intent)
-            finish()
         }
     }
 }
