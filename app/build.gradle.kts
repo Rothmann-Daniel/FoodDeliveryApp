6a3b403a056ab14cbe017aca8f7ffe8ead7e15e4
Daniel Rotman <danilarothmann@gmail.com>
2025-08-14 21:35:28 +0300

    change style Payment Method

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,8 +62,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation ("com.google.android.material:material:1.11.0")
-    implementation("com.google.android.material:material:1.6.0")
-    implementation("com.airbnb.android:lottie:6.3.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation(libs.lottie)
+
 
 }
